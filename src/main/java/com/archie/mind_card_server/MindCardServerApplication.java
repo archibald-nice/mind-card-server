52de14459da9824f1aea2e891a947a887bd91d90
archie <1459553621@qq.com>
2025-07-20 23:22:40 +0800

    first commit

---
+++ src/main/java/com/archie/mind_card_server/MindCardServerApplication.java
@@ -0,0 +1,13 @@
+package com.archie.mind_card_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindCardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindCardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archie/mind_card_server/MindCardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archie.mind_card_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindCardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
